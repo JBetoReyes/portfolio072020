0862567996d37cf9042d1c565b4b990e544a60d9
beto <jose.a.reyes.quevedo@accenture.com>
2020-07-26 18:45:25 -0500

    adding intro and about section

--- src/app/home/sections/IntroSection.component.tsx
+++ src/app/home/sections/IntroSection.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MainSection } from '@common/MainSection.component';
 import { WindowHeader } from '@common/WindowHeader.component';
 import '@styles/pages/Home/sections/_IntroSection.scss';
+import profile from '@assets/profile.png';
 
 export const IntroSection = (): JSX.Element => {
   return (
@@ -18,8 +19,38 @@ export const IntroSection = (): JSX.Element => {
               },
             ]}
           />
-          <div className="right-wrapper">picture</div>
-          <div className="left-wrapper">description</div>
+          <div className="right-wrapper">
+            <div className="right-wrapper__profile-wrapper">
+              <img
+                className="right-wrapper__profile-wrapper--img"
+                src={profile}
+                alt="profileimage"
+              />
+            </div>
+            <div className="right-wrapper__theme-wrapper">
+              <h4 className="right-wrapper__theme-title">Personalize theme</h4>
+              <div className="right-wrapper__theme-dots">
+                <div className="right-wrapper__theme-dot" />
+                <div className="right-wrapper__theme-dot" />
+              </div>
+            </div>
+          </div>
+          <div className="left-wrapper">
+            <div className="left-wrapper__about-wrapper">
+              <div className="left-wrapper__about-wrapper--shadow">
+                <div className="left-wrapper__about-wrapper--corner" />
+                <div className="left-wrapper__about-wrapper--corner" />
+                <div className="left-wrapper__about-wrapper--corner" />
+                <div className="left-wrapper__about-wrapper--corner" />
+                <h3>What I Do?</h3>
+                <p className="left-wrapper__about-content">
+                  I&apos;m a fullstack developer specialized in{' '}
+                  <strong>NodeJS</strong>, <strong>Angular</strong> and{' '}
+                  <strong>ReactJS</strong>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </MainSection>
