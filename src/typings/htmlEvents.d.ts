f5340bfef7ec8daac4a12497156e0ead4be7ba60
beto <josereyes55@hotmail.com>
2020-11-16 22:13:37 -0600

    adding html event types

---
+++ src/typings/htmlEvents.d.ts
@@ -0,0 +1,4 @@
+type InputElements = HTMLInputElement | HTMLTextAreaElement;
+export type AppChangeEvent = React.ChangeEvent<InputElements>;
+export type AppSubmitEvent = React.FormEvent<HTMLFormElement>;
+export type AppClickEvent = React.MouseEvent<HTMLElement, MouseEvent>;
