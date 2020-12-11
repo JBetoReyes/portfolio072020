7590eb3194517cbe68c5dbc1eabe36edd2761324
beto <josereyes55@hotmail.com>
2020-12-10 20:08:15 -0600

    adding style loaders

--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -4,5 +4,13 @@ const baseConfig = require('./webpack.base.config');
 module.exports = (env) => ({
   ...merge(baseConfig(env), {
     mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          loader: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    }
   }),
 });
