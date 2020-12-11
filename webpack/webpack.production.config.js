481f6074bff8543c12a6d625b7031eee6e16b82d
beto <josereyes55@hotmail.com>
2020-12-10 20:27:07 -0600

    fix build command

--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -1,14 +1,27 @@
 const { merge } = require('webpack-merge');
+const HtmlPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { resolve } = require('path');
+
 const baseConfig = require('./webpack.base.config');
+
+const rootPath = resolve(__dirname, '..');
 
 module.exports = (env) => ({
   ...merge(baseConfig(env), {
+    plugins: [
+      new HtmlPlugin({
+        template: resolve(rootPath, 'src', 'public', 'index.html'),
+        filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin()
+    ],
     mode: 'production',
     module: {
       rules: [
         {
           test: /\.s?css$/,
-          loader: ['style-loader', 'css-loader', 'sass-loader'],
+          loader: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
         },
       ],
     }
