3ed60233ab8dde7c4053bf2d5e547894f1e2812b
beto <josereyes55@hotmail.com>
2020-12-21 18:36:56 -0600

    prettiefiying project section file

--- src/frontend/app/home/sections/ProjectsSection.component.tsx
+++ src/frontend/app/home/sections/ProjectsSection.component.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import pokeApp from '@assets/poke-app.png';
 import '@styles/pages/Home/sections/_ProjectsSection.scss';
 
-
 const ProjectSection = (): JSX.Element => {
   return (
     <MainSection>
@@ -11,12 +10,15 @@ const ProjectSection = (): JSX.Element => {
         <h4>PERSONAL PROJECTS</h4>
         <div className="post-wrapper">
           <div className="post">
-            <img className="thumbnail" src={pokeApp} alt="poke-app-project"/>
+            <img className="thumbnail" src={pokeApp} alt="poke-app-project" />
             <div className="post-preview">
-							<h6 className="post-title">Poke App</h6>
-							<p className="post-intro">Small pet project that I created while I was learning React Hooks</p>
-							<a href="https://www.pokeapp.josearq.com">Visit</a>
-						</div>
+              <h6 className="post-title">Poke App</h6>
+              <p className="post-intro">
+                Small pet project that I created while I was learning React
+                Hooks
+              </p>
+              <a href="https://www.pokeapp.josearq.com">Visit</a>
+            </div>
           </div>
         </div>
       </div>
