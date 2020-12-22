add3b35f90aeecc8c1be81c20e9e131c819bb64b
beto <josereyes55@hotmail.com>
2020-12-21 18:44:18 -0600

    adding index server file

---
+++ src/server/index.tsx
@@ -0,0 +1,29 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import dotenv from 'dotenv';
+import { Provider } from 'react-redux';
+import Home from '../frontend/app/home/Home.component';
+import store from '../frontend/app/store/store';
+import getHtmlIndex from './utils/getHtmlIndex';
+
+dotenv.config();
+
+const { ENV: env, PORT: port } = process.env;
+
+(async () => {
+  const app = express();
+  const htmlSkeleton = await getHtmlIndex(env as string);
+  app.get('*', (req, res) => {
+    res.send(
+      renderToString(
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      )
+    );
+  });
+  app.listen(port, () => {
+    console.log(`App running on port: ${port}`);
+  });
+})();
