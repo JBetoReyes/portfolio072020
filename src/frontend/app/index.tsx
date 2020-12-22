59a9b418462e343dc88013008cbaf61b851126e0
beto <josereyes55@hotmail.com>
2020-12-21 18:38:39 -0600

    rolling back front end index

--- src/frontend/app/index.tsx
+++ src/frontend/app/index.tsx
@@ -1,1 +1,13 @@
-console.log('hello from typescript');
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import Home from './home/Home.component';
+import '@styles/Main.scss';
+import store from './store/store';
+
+ReactDom.render(
+  <Provider store={store}>
+    <Home />
+  </Provider>,
+  document.getElementById('app')
+);
